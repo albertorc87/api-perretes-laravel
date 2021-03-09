0b8b55688b0e0333e3283d13d5bba2da8cc53704
Alberto <alberto.r.caballero.87@gmail.com>
2021-03-09 13:15:48 +0100

    first commit

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'v1/auth'
+
+], function ($router) {
+    Route::post('login', [\App\Http\Controllers\Api\V1\AuthController::class, 'login'])->name('login');
+    Route::post('logout', [\App\Http\Controllers\Api\V1\AuthController::class, 'logout'])->name('logout');
+    Route::post('refresh', [\App\Http\Controllers\Api\V1\AuthController::class, 'refresh'])->name('refresh');
+    Route::post('me', [\App\Http\Controllers\Api\V1\AuthController::class, 'me'])->name('me');
+});
